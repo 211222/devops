4251557df53ccf3b2808340f3e79cad1c0eebf2d
211222 <211222@ids.upchiapas.edu.mx>
2024-06-05 17:46:03 -0600

    cambios ultimos x3

--- index.js
+++ index.js
@@ -2,6 +2,6 @@ var http = require('http');
 
 //create a server object:
 http.createServer(function (req, res) {
-  res.write('Hola Danira champo'); //write a response to the client
+  res.write('Ya me quiero ir'); //write a response to the client
   res.end(); //end the response
 }).listen(3001); //the server object listens on port 4000
