f89f8dc125cd60bf9e58146cdb533c15f94742cf
211222 <211222@ids.upchiapas.edu.mx>
2024-06-05 22:10:10 -0600

    hola x5

--- index.js
+++ index.js
@@ -2,6 +2,6 @@ var http = require('http');
 
 //create a server object:
 http.createServer(function (req, res) {
-  res.write('Gracias a todos a los que colaboraron en especial a la Annel'); //write a response to the client
+  res.write('Listo'); //write a response to the client
   res.end(); //end the response
 }).listen(3001); //the server object listens on port 4000
