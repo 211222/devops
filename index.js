d2506cce4d13b126ace6edb873c89880c3b96dc5
211222 <211222@ids.upchiapas.edu.mx>
2024-06-07 12:44:23 -0600

    prueba nueva

--- index.js
+++ index.js
@@ -2,6 +2,6 @@ var http = require('http');
 
 //create a server object:
 http.createServer(function (req, res) {
-  res.write('Listo'); //write a response to the client
+  res.write('Prueba 1'); //write a response to the client
   res.end(); //end the response
 }).listen(3001); //the server object listens on port 4000
