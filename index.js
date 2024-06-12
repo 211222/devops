21ba2b2a80fec02e69d998ca7a798747649584fc
211222 <211222@ids.upchiapas.edu.mx>
2024-06-12 13:51:48 -0600

    practica 3.3

--- index.js
+++ index.js
@@ -2,6 +2,6 @@ var http = require('http');
 
 //create a server object:
 http.createServer(function (req, res) {
-  res.write('Terminado practica 1 y practica 2'); 
+  res.write('Terminado practica 1 y practica 2 y la 3 jeje'); 
   res.end(); 
 }).listen(3000); 
